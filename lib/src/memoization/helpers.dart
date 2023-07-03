9ade81f217828697930d2ad52836fabfce9dc06d
Yakov Karpov <yakovkarpoff@gmail.com>
2023-07-03 15:56:53 +0300

    Add thunk extension

--- lib/src/memoization/helpers.dart
+++ lib/src/memoization/helpers.dart
@@ -1,13 +1,13 @@
 int _combineHashes(int first, int second) => 3 * first + second;
 
 extension on List<Object?> {
-  int get combinedHash => map((e) => e.hashCode).fold(0, _combineHashes);
+  int combineHash() => map((e) => e.hashCode).fold(0, _combineHashes);
 }
 
 extension TryGetMemoized on Function {
   T getMemoizedOrCompute<T>(Map<int, T> results, List<Object?> arguments) =>
       results.putIfAbsent(
-        arguments.combinedHash,
+        arguments.combineHash(),
         () => Function.apply(this, arguments) as T,
       );
 }
